3ff17063ebfdb971707aa12388a95a1512ebaa56
Anuranjan <anuranjanpandey5@gmail.com>
2024-01-16 21:13:10 -0800

    DQN added

--- DQN_PPO.py
+++ DQN_PPO.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
 from ray.rllib.algorithms.ppo import PPOConfig
+from ray.rllib.algorithms.dqn.dqn import DQNConfig
 import numpy as np
 import ray
 
@@ -50,15 +51,30 @@ class DungeonGame(gym.Env):
         return self.cur_pos, reward, terminated, truncated, {}
 
 # Define the training configuration
-config = (
+config_ppo = (
     PPOConfig().environment(
         env=DungeonGame,
-        env_config={"dungeon": [
-            [-2,-3,3],
-            [-5,-10,1],
-            [10,30,-5]
-            ], "initial_health": 50},
-        # env_config={"dungeon": [[-69,40,-16,26,12,-83,-96,-38,-60,-8,14,34,-94,-44,-70,47,-2,-35,-63,-83,7,-60,37,50,32,22,-74,-86,-79,-32,-16,-69,37,27,22,-40,-3,39,26,-50,6,46,-65,-1,39,-19,-87,-88,35,47],[-8,-9,-38,-13,-63,49,-67,-64,-17,-9,-68,-1,-10,-49,8,-59,-34,-85,31,-53,-43,42,-8,48,-25,20,-9,-66,-54,-56,31,-92,-49,40,-91,-62,-14,-41,3,-58,-14,-67,4,38,-62,-41,-59,47,25,-10]], "initial_health": 10000}
+        # env_config={"dungeon": [
+        #     [-2,-3,3],
+        #     [-5,-10,1],
+        #     [10,30,-5]
+        #     ], "initial_health": 50},
+        env_config={"dungeon": [[-69,40,-16,26,12,-83,-96,-38,-60,-8,14,34,-94,-44,-70,47,-2,-35,-63,-83,7,-60,37,50,32,22,-74,-86,-79,-32,-16,-69,37,27,22,-40,-3,39,26,-50,6,46,-65,-1,39,-19,-87,-88,35,47],[-8,-9,-38,-13,-63,49,-67,-64,-17,-9,-68,-1,-10,-49,8,-59,-34,-85,31,-53,-43,42,-8,48,-25,20,-9,-66,-54,-56,31,-92,-49,40,-91,-62,-14,-41,3,-58,-14,-67,4,38,-62,-41,-59,47,25,-10]], "initial_health": 2000}
+    )
+    .rollouts(num_rollout_workers=3)
+)
+
+# Define the training configuration for DQN
+config_dqn = (
+    DQNConfig().environment(
+        env=DungeonGame,
+        # env_config={"dungeon": [
+        #     [-2, -3, 3],
+        #     [-5, -10, 1],
+        #     [10, 30, -5]
+        # ], "initial_health": 50}
+        env_config={"dungeon": [[-69,40,-16,26,12,-83,-96,-38,-60,-8,14,34,-94,-44,-70,47,-2,-35,-63,-83,7,-60,37,50,32,22,-74,-86,-79,-32,-16,-69,37,27,22,-40,-3,39,26,-50,6,46,-65,-1,39,-19,-87,-88,35,47],[-8,-9,-38,-13,-63,49,-67,-64,-17,-9,-68,-1,-10,-49,8,-59,-34,-85,31,-53,-43,42,-8,48,-25,20,-9,-66,-54,-56,31,-92,-49,40,-91,-62,-14,-41,3,-58,-14,-67,4,38,-62,-41,-59,47,25,-10]], "initial_health": 2000}
+
     )
     .rollouts(num_rollout_workers=3)
 )
@@ -67,23 +83,26 @@ config = (
 ray.init()
 
 # Train agent 1
-agent1 = config.build()
+agent1 = config_ppo.build()
 for i in range(20):
     results = agent1.train()
     print(f"Agent 1 - Iter: {i}; avg. reward={results['episode_reward_mean']}")
 
 # Train agent 2
-agent2 = config.build()
+agent2 = config_dqn.build()
 for i in range(20):
     results = agent2.train()
     print(f"Agent 2 - Iter: {i}; avg. reward={results['episode_reward_mean']}")
 
 # Output the trained agents' actions
-env = DungeonGame({"dungeon": [
-    [-2,-3,3],
-    [-5,-10,1],
-    [10,30,-5]
-], "initial_health": 50})
+env = DungeonGame(
+#     {"dungeon": [
+#     [-2,-3,3],
+#     [-5,-10,1],
+#     [10,30,-5]
+# ], "initial_health": 50}
+    {"dungeon": [[-69,40,-16,26,12,-83,-96,-38,-60,-8,14,34,-94,-44,-70,47,-2,-35,-63,-83,7,-60,37,50,32,22,-74,-86,-79,-32,-16,-69,37,27,22,-40,-3,39,26,-50,6,46,-65,-1,39,-19,-87,-88,35,47],[-8,-9,-38,-13,-63,49,-67,-64,-17,-9,-68,-1,-10,-49,8,-59,-34,-85,31,-53,-43,42,-8,48,-25,20,-9,-66,-54,-56,31,-92,-49,40,-91,-62,-14,-41,3,-58,-14,-67,4,38,-62,-41,-59,47,25,-10]], "initial_health": 2000}
+)
 
 obs, info = env.reset()
 terminated = truncated = False
